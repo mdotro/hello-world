984501004a0ad798b981b8a1ec294f5a434ff860
Michael Dotro <mdotro@ucvts.tec.nj.us>
2018-11-06 10:29:46 -0500

    buttons

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+function hc() {
+  console.log("Hello, World!");
+}
+
+function hp() {
+  alert("Hello, World!");
+}
+
+function hb() {
+  var p = document.getElementById('para');
+  p.innerHTML = "Hello, World!";
+}
+
+function hs() {
+  var name = prompt("Please enter your name");
+  console.log("Hello," + name);
+}
